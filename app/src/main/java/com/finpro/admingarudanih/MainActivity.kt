39e655530e044cb0e1b5b64a8477dfa5542d77a5
farelibrahim34 <105421371+farelibrahim34@users.noreply.github.com>
2022-11-28 18:25:12 +0700

    Initial commit

---
+++ app/src/main/java/com/finpro/admingarudanih/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.finpro.admingarudanih
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
