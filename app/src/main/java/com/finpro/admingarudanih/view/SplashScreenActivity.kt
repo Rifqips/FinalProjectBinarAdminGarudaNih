d164a4f9b5c674088b1bae24a253857c0a44ea7d
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2022-12-09 22:23:47 +0700

    tambah fragment create akun admin

--- app/src/main/java/com/finpro/admingarudanih/view/SplashScreenActivity.kt
+++ app/src/main/java/com/finpro/admingarudanih/view/SplashScreenActivity.kt
@@ -6,11 +6,15 @@ import android.os.Bundle
 import android.os.Handler
 import com.finpro.admingarudanih.R
 import com.finpro.admingarudanih.databinding.ActivitySplashScreenBinding
+import com.finpro.admingarudanih.utils.CheckUserUtil
 import com.finpro.admingarudanih.view.auth.LoginActivity
+import com.finpro.admingarudanih.view.home.HomeActivity
+import com.finpro.admingarudanih.viewmodel.AuthViewModel
 
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivitySplashScreenBinding
+    lateinit var authViewModel : AuthViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +23,32 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         Handler().postDelayed({
+//            sudahlogin()
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
         },3000)
+    }
+    private fun sudahlogin(){
+        authViewModel.getToken().observe(this){
+            if (it != null){
+                val validasi = CheckUserUtil.validateUser(it)
+                if (validasi){
+                    startActivity(Intent(this, HomeActivity::class.java))
+                }else{
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
+            }
+        }
+
+        authViewModel.getNoHp().observe(this){
+            if (it != null){
+                if (!it.equals("undefined")){
+                    startActivity(Intent(this, HomeActivity::class.java))
+                }else{
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
+            }
+        }
+
     }
 }
