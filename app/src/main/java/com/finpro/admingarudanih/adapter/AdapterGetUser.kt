bcb7b54352c742b15353a5e999eee50703f4c55c
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2022-12-12 12:41:54 +0700

    tambah detail user tapi belum bisa get image

--- app/src/main/java/com/finpro/admingarudanih/adapter/AdapterGetUser.kt
+++ app/src/main/java/com/finpro/admingarudanih/adapter/AdapterGetUser.kt
@@ -1,5 +1,7 @@
 package com.finpro.admingarudanih.adapter
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,6 +9,7 @@ import com.bumptech.glide.Glide
 import com.finpro.admingarudanih.databinding.ItemGetUserBinding
 import com.finpro.admingarudanih.model.getusers.DataUserItem
 import com.finpro.admingarudanih.model.getusers.User
+import com.finpro.admingarudanih.view.detail.DetailActivity
 
 class AdapterGetUser(val listGetUser: List<User>): RecyclerView.Adapter<AdapterGetUser.ViewHolder>() {
 
@@ -19,15 +22,30 @@ class AdapterGetUser(val listGetUser: List<User>): RecyclerView.Adapter<AdapterG
         return ViewHolder(view)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.tvIdUser.text = listGetUser[position].id.toString()
+        holder.binding.tvIdUser.text ="ID : " + listGetUser[position].id.toString()
         holder.binding.tvEmailUser.text = listGetUser[position].email
         holder.binding.tvUsername.text = listGetUser[position].name
         holder.binding.tvCreated.text = listGetUser[position].createdAt
         holder.binding.tvUpdated.text = listGetUser[position].updatedAt
         holder.binding.tvRoleUser.text = listGetUser[position].role
+        holder.binding.tvPhone.text = listGetUser[position].phone
         Glide.with(holder.itemView.context)
             .load(listGetUser[position].image).into(holder.binding.ivSetImage)
+
+        holder.binding.imgPoster.setOnClickListener {
+            val intent = Intent(it.context, DetailActivity::class.java)
+            intent.putExtra("id", listGetUser[position].id)
+            intent.putExtra("email", listGetUser[position].email)
+            intent.putExtra("username", listGetUser[position].name)
+            intent.putExtra("created", listGetUser[position].createdAt)
+            intent.putExtra("update", listGetUser[position].updatedAt)
+            intent.putExtra("role", listGetUser[position].role)
+            intent.putExtra("phone", listGetUser[position].phone)
+            intent.putExtra("image", listGetUser[position].image)
+            it.context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
---
+++ app/src/main/java/com/finpro/admingarudanih/view/detail/DetailActivity.kt
@@ -0,0 +1,49 @@
+package com.finpro.admingarudanih.view.detail
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.finpro.admingarudanih.databinding.ActivityDetailBinding
+import com.finpro.admingarudanih.view.home.HomeActivity
+import dagger.hilt.android.AndroidEntryPoint
+import de.hdodenhof.circleimageview.CircleImageView
+
+@AndroidEntryPoint
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getUser()
+        binding.ivBack.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun getUser(){
+        val itemUser = intent
+
+        val id = itemUser.getIntExtra("id",0)
+        val email = itemUser.getStringExtra("email")
+        val username = itemUser.getStringExtra("username")
+        val created = itemUser.getStringExtra("created")
+        val update = itemUser.getStringExtra("update")
+        val role = itemUser.getStringExtra("role")
+        val phone = itemUser.getStringExtra("phone")
+//        val image = itemUser.getStringExtra("image").toString().toInt()
+
+        binding.tvIdUser.text = "ID : $id"
+        binding.tvEmailUser.text = emaild
+        binding.tvUsername.text = username
+        binding.tvCreated.text = created
+        binding.tvUpdated.text = update
+        binding.tvRoleUser.text = role
+        binding.tvPhone.text = phone
+//        binding.ivSetImage.setImageResource(image)
+    }
+}
